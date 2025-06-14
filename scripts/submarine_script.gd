8a92130496c0d7ba14d42f7252b789d8a825d4e5
radielay <radiela.y@gmail.com>
2025-06-14 15:29:41 +0100

    - camera fix

--- scripts/submarine_script.gd
+++ scripts/submarine_script.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
 var move_speed = 100
+#var camera = Camera2D.new()
 
 func _ready() -> void:
 	gravity_scale = 0.1
+	#camera.look_at()
 	
 func _input(event: InputEvent) -> void:
-	var impulse = Vector2(0, -1) 
+	var impulse = Vector2(0, -1) 	
 	var left_impulse = Vector2(-1, 0)
 	var right_impulse = Vector2(1, 0)
 	
