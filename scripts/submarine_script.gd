fe03f2206180f53034e999e0034952276261092c
radielay <radiela.y@gmail.com>
2025-06-14 14:34:38 +0100

    Player & level

---
+++ scripts/submarine_script.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+var move_speed = 100
+
+func _ready() -> void:
+	gravity_scale = 0.1
+	
+func _input(event: InputEvent) -> void:
+	var impulse = Vector2(0, -1) 
+	var left_impulse = Vector2(-1, 0)
+	var right_impulse = Vector2(1, 0)
+	
+	if event.is_action("ui_up"):
+		apply_impulse(impulse*move_speed)
+		
+	if event.is_action("ui_left"):
+		apply_impulse(left_impulse*move_speed)
+	
+	if event.is_action("ui_right"):
+		apply_impulse(right_impulse*move_speed)
+
+
+func _on_body_entered(body: Node) -> void:
+	queue_free()
+	
