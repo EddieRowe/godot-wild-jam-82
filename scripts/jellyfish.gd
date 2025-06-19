b78ff96bc6d41aa7e1e0b08f3ef5a45fc2468858
tentcoding <68378134+EddieRowe@users.noreply.github.com>
2025-06-19 20:59:44 +0100

    jellyfish animation fix
    - added string movement_animation_name to jellyfish script
    - entered correct movement_animation_names in editor (jellyfish.tscn, jellyfish_tiny2.tscn)
    - added on_body_entered signal to jellyfish_tiny2.tscn
    - removed jellyfish_tiny.gd as was duplicate of jellyfish.gd

--- scripts/jellyfish.gd
+++ scripts/jellyfish.gd
@@ -3,10 +3,11 @@ extends RigidBody2D
 var speed : int = 10
 var player : PlayerMovement
 @export var animation_player : AnimationPlayer
+@export var movement_animation_name : String
 
 func _ready() -> void:
 	player = get_parent().get_node("Player/RigidBody2D")
-	animation_player.play("jellyfish_move")
+	animation_player.play(movement_animation_name)
 
 func _physics_process(delta: float) -> void:
 	var move_direction =  player.global_position - global_position
