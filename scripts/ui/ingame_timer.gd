4ee727abe2a8f1a8d58ea0345bbfe75340007a11
tentcoding <68378134+EddieRowe@users.noreply.github.com>
2025-06-22 14:04:39 +0100

    add level timer ui

---
+++ scripts/ui/ingame_timer.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+var current_run_time : float = 0.0
+@onready var timer_rich_text_label: RichTextLabel = $PanelContainer/TimerRichTextLabel
+var running = true
+@onready var animation_player: AnimationPlayer = $PanelContainer/TimerRichTextLabel/AnimationPlayer
+
+func _ready() -> void:
+	var player : PlayerMovement = get_parent().get_node("Player/RigidBody2D")
+	player.health.healthChanged.connect(_player_health_update)
+	animation_player.play("timer_run")
+
+func _player_health_update(newHealth : int) -> void:
+	if newHealth <= 0 and running:
+		running = false
+		animation_player.play("timer_dead")
+
+func _process(delta: float) -> void:
+	current_run_time += delta
+	_update_ui()
+	
+func _update_ui() -> void:
+	if (!running):
+		return
+	timer_rich_text_label.text = _format_float_to_string(current_run_time)
+
+func _format_float_to_string(time : float) -> String:
+	var seconds : float = fmod(time , 60.0)
+	var minutes : int   =  int(time / 60.0) % 60
+	var mmss_string : String = "%02d:%05.2f" % [minutes, seconds]
+	return mmss_string
+
+func _pause_timer() -> void:
+	running = false
