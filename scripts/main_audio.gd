318cdba2283b667a8212cbf2ef84910cdd6b11b0
tentcoding <68378134+EddieRowe@users.noreply.github.com>
2025-06-22 14:36:34 +0100

    use muffled music before game starts

--- scripts/main_audio.gd
+++ scripts/main_audio.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 var music_audio_stream_player : AudioStreamPlayer
 
+@export var main_music : AudioStream
+
 func _ready() -> void:
 	music_audio_stream_player = get_node("MusicAudioStreamPlayer")
 	var player : PlayerMovement = get_parent().get_node("Player/RigidBody2D")
 	player.health.healthChanged.connect(_player_health_update)
+	player.game_started_signal.connect(_game_start)
+
+func _game_start():
+	music_audio_stream_player.stream = main_music
+	music_audio_stream_player.play()
 
 func _player_health_update(newHealth : int) -> void:
 	if newHealth <= 0 and music_audio_stream_player.playing:
